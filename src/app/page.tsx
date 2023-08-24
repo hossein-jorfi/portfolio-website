5f71748b1f76b8665365cfdba57fa9f9438f48f7
hosseinjorfi <hosseinjorfi81@gmail.com>
2023-08-24 08:33:20 +0330

    profile image changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import picture from "../../public/images/developer-pic-1.png";
+import picture2 from "../../public/images/myPic.jpg";
 import AnimatedText from "@/components/AnimatedText";
 
 function Home() {
   return (
     <div className="flex items-center text-dark w-full min-h-screen z-0 bg-light px-32 2xl:container">
       <div className="flex items-center justify-between w-full">
-        <div className="w-1/2">
-          <Image src={picture} alt="profile" className="w-full h-auto" />
+        <div className="w-1/2 rounded-full full ">
+          <Image src={picture2} alt="profile" className="w-5/6 rounded-full h-auto border-4 border-black" />
         </div>
         <div className="w-1/2">
           <AnimatedText className="" title="Hossein Jorfi" />
