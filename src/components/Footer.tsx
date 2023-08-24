c27b0ee9ccdd1fdd28d8afad01a98fac40b9cbd3
hosseinjorfi <hosseinjorfi81@gmail.com>
2023-08-24 15:38:51 +0330

    fooer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import heartGif from "../../public/gif/icons8-heart.gif";
 
 const Footer = () => {
   return (
-    <footer className="my-container border-t-2 flex justify-center items-center border-dark py-6">
+    <footer className="my-container border-t-4 flex justify-center items-center border-dark py-6">
       <p className="text-dark font-semibold flex items-center">
         Build with{" "}
         <span className="px-2 w-11">
