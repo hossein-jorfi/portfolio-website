f0a280395d5a633cac7d18aa6d113cb83a93d53e
hosseinjorfi <hosseinjorfi81@gmail.com>
2023-09-24 15:31:05 +0330

    icons

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -325,7 +325,7 @@ export const MoonIcon = ({ className, ...rest }: { className: string }) => (
       strokeWidth="2"
     >
       <path d="M7 6 C7 12.08 11.92 17 18 17 C18.53 17 19.05 16.96 19.56 16.89 C17.95 19.36 15.17 21 12 21 C7.03 21 3 16.97 3 12 C3 8.83 4.64 6.05 7.11 4.44 C7.04 4.95 7 5.47 7 6 Z" />
-      <set attributeName="opacity" begin="0.6s" to="0" />
+      {/* <set attributeName="opacity" begin="0.6s" to="0" /> */}
     </g>
     <mask id="lineMdMoonFilledToSunnyFilledLoopTransition0">
       <circle cx="12" cy="12" r="12" fill="#fff" />
@@ -384,7 +384,7 @@ export const MoonIcon = ({ className, ...rest }: { className: string }) => (
       mask="url(#lineMdMoonFilledToSunnyFilledLoopTransition0)"
       opacity="0"
     >
-      <set attributeName="opacity" begin="0.6s" to="1" />
+      {/* <set attributeName="opacity" begin="0.6s" to="1" /> */}
       <animate
         fill="freeze"
         attributeName="r"
