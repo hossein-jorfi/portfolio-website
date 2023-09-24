82b6c5d3263b69f4867f36dbd345e1491e271a98
hosseinjorfi <hosseinjorfi81@gmail.com>
2023-09-24 11:30:31 +0330

    project page created and some style

---
+++ src/app/Projects/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+      return (
+            <div className='my-container min-h-screen'>
+                  Projects
+            </div>
+      );
+};
+
+export default page;
