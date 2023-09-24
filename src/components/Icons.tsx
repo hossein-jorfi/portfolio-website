6423189ca0b8c30e393bf861c3898a1187a7a390
hosseinjorfi <hosseinjorfi81@gmail.com>
2023-09-24 15:34:23 +0330

    icons

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -102,7 +102,6 @@ export const SunIcon = ({ className, ...rest }: { className: string }) => (
         d="M7 6 C7 12.08 11.92 17 18 17 C18.53 17 19.05 16.96 19.56 16.89 C17.95 19.36 15.17 21 12 21 C7.03 21 3 16.97 3 12 C3 8.83 4.64 6.05 7.11 4.44 C7.04 4.95 7 5.47 7 6 Z"
         opacity="0"
       >
-        <set attributeName="opacity" begin="0.5s" to="1" />
       </path>
     </g>
     <g fill="currentColor" fillOpacity="0">
@@ -228,7 +227,6 @@ export const SunIcon = ({ className, ...rest }: { className: string }) => (
       fill="currentColor"
       mask="url(#lineMdSunnyFilledLoopToMoonFilledLoopTransition1)"
     >
-      <set attributeName="opacity" begin="0.5s" to="0" />
       <animate
         fill="freeze"
         attributeName="r"
