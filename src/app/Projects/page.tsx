94e4292cef999c51b823dd0ae50b4342580ac4d7
hosseinjorfi <hosseinjorfi81@gmail.com>
2023-09-26 17:13:44 +0330

    projects section

--- src/app/Projects/page.tsx
+++ src/app/Projects/page.tsx
@@ -1,13 +1,18 @@
 "use client";
+import ProjectCard from "@/components/ProjectCard";
 import TransitionEffect from "@/components/TransitionEffect";
 import React from "react";
 
 const page = () => {
   return (
     <>
-      <TransitionEffect />
-      <div className="my-container min-h-screen bg-light dark:bg-dark">
-        Projects
+      {/* <TransitionEffect /> */}
+      <div className="pb-10 my-container min-h-screen bg-light dark:bg-dark grid grid-cols-12 gap-3 -z-20">
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard /> 
       </div>
     </>
   );
---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import myPic2 from "../../public/images/about_page_photo.jpg";
+
+const ProjectCard = () => {
+  return (
+    <div className="w-full h-max absolute col-span-12 sm:col-span-6 rounded-2xl border-2 border-solid border-dark dark:border-light bg-light dark:bg-dark p-4">
+      <div className="relative top-0 -right-3 w-[102%] h-[103%] rounded-[1.5rem] bg-dark "></div>
+      <div className="">
+        <Image
+          src={myPic2}
+          alt="my photo"
+          className="rounded-2xl w-full h-auto"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
