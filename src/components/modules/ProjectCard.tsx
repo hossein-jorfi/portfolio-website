628fdffe213f313f8d3f17bbd8207ac49577ce7d
hosseinjorfi <hosseinjorfi81@gmail.com>
2023-10-01 15:24:11 +0330

    project cart

--- src/components/modules/ProjectCard.tsx
+++ src/components/modules/ProjectCard.tsx
@@ -1,13 +1,20 @@
 import Image from "next/image";
 import myPic2 from "../../../public/images/about_page_photo.jpg";
 
-const ProjectCard = () => {
+const ProjectCard = ({ icon }: {icon: any }) => {
   return (
-    <div className="w-full h-max relative col-span-12 sm:col-span-6 rounded-2xl border-2 border-solid border-dark dark:border-light bg-light dark:bg-dark p-4">
+    <div className="mt-7 sm:mt-0 w-full h-max relative sm:col-span-6 rounded-2xl border-2 border-solid border-dark dark:border-light bg-light dark:bg-dark p-4">
+      <div className="absolute top-0 -right-3 w-[102%] h-[103%] rounded-[1.5rem] bg-dark dark:bg-light -z-10"></div>
 
-      <div className="absolute top-0 -right-3 w-[102%] h-[103%] rounded-[1.5rem] bg-dark -z-10"></div>
-
-      <div className="">
+      <div className="flex justify-between items-center">
+        <p className="font-bold text-dark dark:text-light">Project</p>
+        <div className="w-4">
+          <svg viewBox="0 0 128 128">
+            {icon}
+          </svg>
+        </div>
+      </div>
+      <div className="mt-2">
         <Image
           src={myPic2}
           alt="my photo"
