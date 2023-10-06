11c3701976ab74bae1b90ec33543bf2f09231108
hosseinjorfi <hosseinjorfi81@gmail.com>
2023-10-06 15:01:38 +0330

    Crypto project detail page

--- src/components/templates/projects/Crypto.tsx
+++ src/components/templates/projects/Crypto.tsx
@@ -1,5 +1,24 @@
+import Image from "next/image";
+import cryptoImage from "../../../../public/projectsImages/Crypto/crypto.png";
+import ProjectDetailLink from "@/components/elements/ProjectDetailLink";
+import AnimatedText from "@/components/elements/AnimatedText";
+
 const Crypto = () => {
-  return <div>Crypto</div>;
+  return (
+    <div className="my-container min-h-screen w-full dark:bg-dark pb-32">
+      <div>
+        <Image src={cryptoImage} alt="project" />
+      </div>
+      <ProjectDetailLink
+        demo="https://jolly-kangaroo-e551b5.netlify.app/BTC"
+        github="https://github.com/hossein-jorfi/crypto"
+      />
+      <div className="mt-10">
+        <AnimatedText title="Description" className="" />
+        <p className="text-xl text-center text-dark dark:text-light">This site show data of the coin dynamically. if you put symbol of the coin in end point you can see information about the coin like domain/BTC domain/ETH. i used Next.js and bootstrap to make it. its use 5 api to get and show data. 2 api for coin informaion and the icon, one for chart, one for convert section, and the wordpress RESTapi for comment section and articles section. the articles in wordpress weblog that related to coin get from api and same for the comments (its not working in the demo. but you can see the methods in code)</p>
+      </div>
+    </div>
+  );
 };
 
 export default Crypto;
