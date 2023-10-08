1637a8f5faee9cec15b998b75a6ef90109ee3688
hosseinjorfi <hosseinjorfi81@gmail.com>
2023-10-08 12:45:58 +0330

    fixig responsive isuses

--- src/components/modules/ProjectCard.tsx
+++ src/components/modules/ProjectCard.tsx
@@ -14,27 +14,29 @@ const ProjectCard = ({
   image: StaticImageData;
 }) => {
   return (
-    <Link href={`/Projects/${routeId}`} className="mt-7 sm:mt-0 w-full h-max relative sm:col-span-6 rounded-2xl border-2 border-solid border-dark dark:border-light bg-light dark:bg-dark p-4">
-      <div className="absolute top-0 -right-3 w-[102%] h-[103%] rounded-[1.5rem] bg-dark dark:bg-light -z-10"></div>
+    <div className="mt-7 sm:mt-0 h-max w-full relative sm:col-span-6 rounded-2xl border-2 border-solid border-dark dark:border-light bg-light dark:bg-dark p-4">
+      <Link href={`/Projects/${routeId}`} className="">
+        <div className="absolute top-0 -right-3 w-[102%] h-[103%] rounded-[1.5rem] bg-dark dark:bg-light -z-10"></div>
 
-      <div className="flex justify-between items-center">
-        <p className="font-bold text-dark dark:text-light">{title}</p>
-        <div className="flex space-x-1">
-          {icons.map((item: { icon: any; id: number }) => (
-            <div key={item.id} className="w-4">
-              <svg viewBox="0 0 128 128">{item.icon}</svg>
-            </div>
-          ))}
+        <div className="flex justify-between items-center">
+          <p className="font-bold text-dark dark:text-light">{title}</p>
+          <div className="flex space-x-1">
+            {icons.map((item: { icon: any; id: number }) => (
+              <div key={item.id} className="w-4">
+                <svg viewBox="0 0 128 128">{item.icon}</svg>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-      <div className="mt-2">
-        <Image
-          src={image}
-          alt="my photo"
-          className="rounded-2xl w-full h-auto"
-        />
-      </div>
-    </Link>
+        <div className="mt-2">
+          <Image
+            src={image}
+            alt="my photo"
+            className="rounded-2xl w-full h-auto"
+          />
+        </div>
+      </Link>
+    </div>
   );
 };
 
