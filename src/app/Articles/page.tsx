2642b3a7e4aca0b381b671337ee11743c754dcee
hosseinjorfi <hosseinjorfi81@gmail.com>
2023-10-09 11:53:29 +0330

    some  changes

---
+++ src/app/Articles/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div className="my-container min-h-screen">
+      <p>Nothing yet...</p>
+    </div>
+  );
+};
+
+export default page;
--- src/components/templates/Home.tsx
+++ src/components/templates/Home.tsx
@@ -25,7 +25,7 @@ const HomePage = () => {
           <p className="font-medium text-base">
             As a skilled Front-End developer, I am dedicated to turning ideas
             into innovative web applications. Explore my latest projects and
-            articles, showcasing my expertise in React.js and web development.
+            articles, showcasing my expertise in ( Next.js / React.js ) and web development.
           </p>
           <div className="flex items-center self-start mt-4">
             <Link
