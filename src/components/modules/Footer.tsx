6807de3f2ef0b3daf1a7d6d16e7e4c14428f6db7
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-03-26 09:48:01 +0330

    fix footer line length in wide displays

--- src/components/modules/Footer.tsx
+++ src/components/modules/Footer.tsx
@@ -3,15 +3,18 @@ import heartGif from "../../../public/gif/heart.png";
 
 const Footer = () => {
   return (
-    <footer className="my-container border-t-4 flex justify-center items-center border-dark dark:border-light bg-light dark:bg-dark py-6">
-      <p className="text-dark dark:text-light font-semibold flex items-center">
-        Build with{" "}
-        <span className="px-2 w-11">
-          <Image src={heartGif} alt="heart" className="rounded-full" />
-        </span>{" "}
-        By Hossein Jorfi
-      </p>
-    </footer>
+    <>
+      <div className="bg-dark dark:bg-light w-full h-[4px]"></div>
+      <footer className="my-container flex justify-center items-center bg-light dark:bg-dark py-6">
+        <p className="text-dark dark:text-light font-semibold flex items-center">
+          Build with{" "}
+          <span className="px-2 w-11">
+            <Image src={heartGif} alt="heart" className="rounded-full" />
+          </span>{" "}
+          By Hossein Jorfi
+        </p>
+      </footer>
+    </>
   );
 };
 
