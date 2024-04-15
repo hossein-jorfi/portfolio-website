0d180fb930c540f234a2b8a8b91a2054a8a4e469
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-04-15 10:59:32 +0330

    show images in botlyzer detail page

--- src/components/templates/projects/Botlyzer.tsx
+++ src/components/templates/projects/Botlyzer.tsx
@@ -1,7 +1,18 @@
 import Image from "next/image";
-import MainImage from "../../../../public/projectsImages/BotLyzer/bot1.png";
 import ProjectDetailLink from "@/components/elements/ProjectDetailLink";
 import AnimatedText from "@/components/elements/AnimatedText";
+// Images
+import MainImage from "../../../../public/projectsImages/BotLyzer/bot1.png";
+import autoTradingImage from "../../../../public/projectsImages/BotLyzer/bot_autotrading.png";
+import login from "../../../../public/projectsImages/BotLyzer/bot_login.png";
+import tradeSimImage from "../../../../public/projectsImages/BotLyzer/bot_trade_sim.png";
+import createStartegyImage from "../../../../public/projectsImages/BotLyzer/bot_c_srategy.png";
+import modalImage from "../../../../public/projectsImages/BotLyzer/bot_modal.png";
+import signalImage from "../../../../public/projectsImages/BotLyzer/boy_signal.png";
+import traderBotsImage from "../../../../public/projectsImages/BotLyzer/bot_trader_bots.png";
+import pricingImage from "../../../../public/projectsImages/BotLyzer/bot_pricing.png";
+import pricing2Image from "../../../../public/projectsImages/BotLyzer/bot_pricong_2.png";
+import portfolioImage from "../../../../public/projectsImages/BotLyzer/bot_portfolio.png";
 
 const Botlyzer = () => {
   return (
@@ -25,6 +36,24 @@ const Botlyzer = () => {
             Routing - API - WebSocket - State Management(Redux & Context) -
             Performance Pptimization And...
           </p>
+        </div>
+      </div>
+      <div className="mt-5">
+        <AnimatedText
+          title="Images"
+          className="text-left !text-3xl sm:!text-5xl"
+        />
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+          <Image src={autoTradingImage} alt="project image" />
+          <Image src={tradeSimImage} alt="project image" />
+          <Image src={createStartegyImage} alt="project image" />
+          <Image src={traderBotsImage} alt="project image" />
+          <Image src={signalImage} alt="project image" />
+          <Image src={login} alt="project image" />
+          <Image src={modalImage} alt="project image" />
+          <Image src={pricingImage} alt="project image" />
+          <Image src={pricing2Image} alt="project image" />
+          <Image src={portfolioImage} alt="project image" />
         </div>
       </div>
     </div>
