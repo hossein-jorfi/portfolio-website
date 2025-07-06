55e1f62195cbde663624f9536f55be9a5ec1564d
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-07-06 21:24:53 +0330

    logo

--- src/components/shared/layout/header/index.tsx
+++ src/components/shared/layout/header/index.tsx
@@ -1,9 +1,11 @@
 import ThemeToggle from "@/components/shared/theme-toggle";
+import { Button } from "@/components/ui/button";
+import { Code } from "lucide-react";
 
 const Header = () => {
   return (
     <div className="app-container py-4 flex items-center justify-between">
-      logo
+      <Button variant="ghost" size="icon"><Code /></Button>
       <ThemeToggle />
     </div>
   );
