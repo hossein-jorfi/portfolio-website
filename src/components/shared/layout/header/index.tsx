ac6cc0a2e39f5c76ab3eb7feefa92fa5c81a7262
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-07-06 21:13:06 +0330

    Header styless

--- src/components/shared/layout/header/index.tsx
+++ src/components/shared/layout/header/index.tsx
@@ -2,7 +2,8 @@ import ThemeToggle from "@/components/shared/theme-toggle";
 
 const Header = () => {
   return (
-    <div className="app-container">
+    <div className="app-container py-4 flex items-center justify-between">
+      logo
       <ThemeToggle />
     </div>
   );
