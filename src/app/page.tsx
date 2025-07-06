11b0979d632ba3f1dc1ee257b9559ef0348fe79f
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-07-06 20:37:50 +0330

    use src

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "../ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Button>Button</Button>
+      <Button variant="secondary">Button</Button>
+      <Button variant="outline">Button</Button>
+      <Button variant="destructive">Button</Button>
+      <Button variant="link">Button</Button>
+      <Button variant="ghost">Button</Button>
+    </div>
+  );
+}
