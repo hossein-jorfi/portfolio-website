ff3473f964ee9578ea7a0b4f657dcb0642ef84cf
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-07-11 10:42:45 +0330

    Download CV nutton

--- src/components/shared/layout/header/index.tsx
+++ src/components/shared/layout/header/index.tsx
@@ -1,11 +1,19 @@
 import ThemeToggle from "@/components/shared/theme-toggle";
 import Menu from "./menu";
+import { Button } from "@/components/ui/button";
+import { DownloadIcon } from "lucide-react";
 
 const Header = () => {
   return (
     <div className="app-container py-4 flex items-center justify-between">
       <Menu />
-      <ThemeToggle />
+
+      <div className="flex h-4 items-center space-x-3">
+        <Button size="sm">
+          <DownloadIcon /> Download CV
+        </Button>
+        <ThemeToggle />
+      </div>
     </div>
   );
 };
--- src/components/shared/layout/header/menu.tsx
+++ src/components/shared/layout/header/menu.tsx
@@ -1,11 +1,11 @@
 import { Button } from "@/components/ui/button";
-import { Code } from "lucide-react";
+import { CodeXml } from "lucide-react";
 
 const Menu = () => {
   return (
     <div className="flex items-center gap-2">
       <Button variant="ghost" size="icon">
-        <Code />
+        <CodeXml className="size-5" />
       </Button>
       {Array.from({ length: 5 }).map((_, index) => (
         <Button variant="ghost" size="sm" className="font-semibold" key={index}>
