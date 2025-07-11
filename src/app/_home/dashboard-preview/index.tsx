db5e58f85b9ea6739bff19445fcaaa181cd7f3eb
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-07-11 14:11:47 +0330

    DashboardPreview

--- src/app/_home/dashboard-preview/index.tsx
+++ src/app/_home/dashboard-preview/index.tsx
@@ -1,12 +1,26 @@
+import { Button } from "@/components/ui/button";
+import { Muted } from "@/components/ui/typography";
+import Link from "next/link";
+
 const DashboardPreview = () => {
   return (
-    <div className="overflow-hidden w-full mt-20 mb-10 rounded-lg">
-      <iframe
-        src="https://ui.shadcn.com/view/dashboard-01"
-        height="1000px"
-        loading="lazy"
-        className="bg-background no-scrollbar relative z-20 w-full"
-      ></iframe>
+    <div className="w-full mt-20 mb-10">
+      <Muted>
+        This is a preview of a dashboard created with{" "}
+        <Button variant="link" asChild className="p-0 px-0.5">
+          <Link href="https://ui.shadcn.com/blocks" target="_blank">
+            shadcn
+          </Link>
+        </Button>
+      </Muted>
+      <div className="overflow-hidden w-full rounded-lg mt-0.5">
+        <iframe
+          src="https://ui.shadcn.com/view/dashboard-01"
+          height="1000px"
+          loading="lazy"
+          className="bg-background no-scrollbar relative z-20 w-full"
+        ></iframe>
+      </div>
     </div>
   );
 };
