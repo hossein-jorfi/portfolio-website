059daf87fa378847cc5c34dcfc25d5afd4cbb4cd
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-07-11 11:49:07 +0330

    home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import { H1 } from "@/components/ui/typography";
+import { Badge } from "@/components/ui/badge";
+import { H1, H3 } from "@/components/ui/typography";
 
 export default function Home() {
   return (
-    <div className="app-container mt-30 flex flex-col items-center gap-10">
+    <div className="app-container mt-30 flex flex-col items-center gap-2">
+      <Badge variant="secondary">Front End Developer</Badge>
       <H1>Hossein Jorfi</H1>
+      <H3>I Can Build High Quality, Tested and Scalable Web Applications.</H3>
     </div>
   );
 }
