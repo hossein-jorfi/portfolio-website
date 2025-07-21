376ae7a4bac5d49fbbe0ab71033254adb36f16b3
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-07-21 12:23:51 +0330

    ExperienceItem

--- src/app/experience/_components/experience-item/index.tsx
+++ src/app/experience/_components/experience-item/index.tsx
@@ -1,5 +1,56 @@
-const ExperienceItem = () => {
-  return <div>ExperienceItem</div>;
+interface Project {
+      name: string;
+      description: string;
+    }
+
+export interface ExperienceItemProps {
+  company: string;
+  position: string;
+  jalaliDate: {
+    start: string;
+    end: string;
+  };
+  englishDate: {
+    start: string;
+    end: string;
+  };
+  description: string;
+  projects: Project[];
+}
+
+const ExperienceItem = ({
+  company,
+  position,
+  jalaliDate,
+  englishDate,
+  description,
+  projects,
+}: ExperienceItemProps) => {
+  return (
+    <div className="mt-10">
+      <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-1">
+          <h3 className="text-lg font-bold">{company}</h3>
+          <p className="text-sm text-gray-500">{position}</p>
+          <p className="text-sm text-gray-500">
+            {jalaliDate.start} - {jalaliDate.end}
+          </p>
+          <p className="text-sm text-gray-500">
+            {englishDate.start} - {englishDate.end}
+          </p>
+        </div>
+        <p className="text-sm text-gray-500">{description}</p>
+        <div className="flex flex-col gap-2">
+          {projects.map((project) => (
+            <div key={project.name}>
+              <h4 className="text-lg font-bold">{project.name}</h4>
+              <p className="text-sm text-gray-500">{project.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ExperienceItem;
--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,9 +1,64 @@
-import ExperienceItem from "./_components/experience-item";
+import ExperienceItem, {
+  ExperienceItemProps,
+} from "./_components/experience-item";
+
+const experienceItems: ExperienceItemProps[] = [
+  {
+    company: "Company 1",
+    position: "Position 1",
+    jalaliDate: {
+      start: "1402/01/01",
+      end: "1402/01/01",
+    },
+    englishDate: {
+      start: "2023/01/01",
+      end: "2023/01/01",
+    },
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci omnis, error necessitatibus quod sequi accusamus provident, pariatur commodi doloremque quibusdam a vel unde harum architecto eaque, fuga cumque laborum fugiat.",
+    projects: [
+      {
+        name: "Project 1",
+        description: "Description 1",
+      },
+      {
+        name: "Project 2",
+        description: "Description 2",
+      },
+    ],
+  },
+  {
+    company: "Company 2",
+    position: "Position 2",
+    jalaliDate: {
+      start: "1402/01/01",
+      end: "1402/01/01",
+    },
+    englishDate: {
+      start: "2023/01/01",
+      end: "2023/01/01",
+    },
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci omnis, error necessitatibus quod sequi accusamus provident, pariatur commodi doloremque quibusdam a vel unde harum architecto eaque, fuga cumque laborum fugiat.",
+    projects: [
+      {
+        name: "Project 1",
+        description: "Description 1",
+      },
+      {
+        name: "Project 2",
+        description: "Description 2",
+      },
+    ],
+  },
+];
 
 const Page = () => {
   return (
     <div className="app-container">
-      <ExperienceItem />
+      {experienceItems.map((item) => (
+        <ExperienceItem key={item.company} {...item} />
+      ))}
     </div>
   );
 };
