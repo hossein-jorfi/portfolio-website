317d26121e0b2c4b60437f089f90b4c61da2a7d5
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-07-24 11:12:04 +0330

    ProjectItem

--- src/app/experience/_components/experience-item/project-item.tsx
+++ src/app/experience/_components/experience-item/project-item.tsx
@@ -1,17 +1,30 @@
-import { H4, Muted, Small } from "@/components/ui/typography";
+import { H4, Large, Muted, Small } from "@/components/ui/typography";
 import { Project } from "../../types";
+import { CheckCircle } from "lucide-react";
 
-const ProjectItem = ({ name, description }: Project) => {
+const ProjectItem = ({ name, description, responsibilities }: Project) => {
   return (
     <div className="grid grid-cols-3 gap-7">
-      <div className="space-y-3 col-span-2">
+      <div className="space-y-4 col-span-2">
         <div className="space-y-1">
           <H4>{name}</H4>
           <Muted>{description}</Muted>
         </div>
-        <div>
-          <Small>My Responsilties</Small>
-        </div>
+        {responsibilities && (
+          <div className="space-y-3">
+            <Large>My Responsilties</Large>
+            <div className="space-y-2">
+              {responsibilities.map((responsibility) => (
+                <Small
+                  key={responsibility}
+                  className="flex items-center gap-2 text-foreground/80"
+                >
+                  <CheckCircle className="size-4" /> {responsibility}
+                </Small>
+              ))}
+            </div>
+          </div>
+        )}
       </div>
       <div className="border rounded-lg w-full h-[300px]" />
     </div>
