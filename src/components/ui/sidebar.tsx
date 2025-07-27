1dccf907ea0286d423c15ea3d5c7dd0478491709
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-07-27 22:23:29 +0330

    delete min-h-svh class from sidebar provider

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -139,7 +139,8 @@ function SidebarProvider({
             } as React.CSSProperties
           }
           className={cn(
-            "group/sidebar-wrapper has-data-[variant=inset]:bg-sidebar flex min-h-svh w-full",
+            "group/sidebar-wrapper has-data-[variant=inset]:bg-sidebar flex w-full",
+            // "min-h-svh"
             className
           )}
           {...props}
