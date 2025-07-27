7c1373b3c77e0512a851b7ffb2e2c5a98190c690
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-07-27 22:19:21 +0330

    MobileMenu

--- src/components/shared/layout/header/menu/mobile-menu.tsx
+++ src/components/shared/layout/header/menu/mobile-menu.tsx
@@ -1,13 +1,51 @@
-import { Button } from "@/components/ui/button";
-import { Menu } from "lucide-react";
+"use client";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { menuItems } from ".";
+import Link from "next/link";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 const MobileMenu = () => {
+  const isMobile = useIsMobile();
+
+  if (!isMobile) return null;
+
   return (
-    <div className="md:hidden">
-      <Button variant="ghost" size="icon">
-        <Menu />
-      </Button>
-    </div>
+    <SidebarProvider>
+        <SidebarTrigger />
+
+        <Sidebar>
+          <SidebarContent>
+            <SidebarGroup>
+              <SidebarGroupLabel>Menu</SidebarGroupLabel>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  {menuItems.map((item) => (
+                    <SidebarMenuItem key={item.label}>
+                      <SidebarMenuButton asChild>
+                        <Link href={item.href}>
+                          <span>{item.label}</span>
+                        </Link>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  ))}
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </SidebarGroup>
+          </SidebarContent>
+        </Sidebar>
+    </SidebarProvider>
   );
 };
 
