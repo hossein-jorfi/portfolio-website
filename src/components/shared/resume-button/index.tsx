cde3d5bc3f0618fb85877366e64e81ef674abb97
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-08-01 17:54:22 +0330

    ResumeButton

--- src/components/shared/resume-button/index.tsx
+++ src/components/shared/resume-button/index.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button";
 import { DownloadIcon } from "lucide-react";
+import Link from "next/link";
 
 const ResumeButton = () => {
   return (
-    <Button size="sm">
-      <DownloadIcon /> Resume
+    <Button size="sm" asChild>
+      <Link href="/Hossein_Jorfi.pdf" download={true}>
+        <DownloadIcon /> Resume
+      </Link>
     </Button>
   );
 };
