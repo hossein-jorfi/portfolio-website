cc8fd36270726eec6ff9779ed7c3d46714ddca3e
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-08-01 17:33:20 +0330

    ContactButton component

---
+++ src/app/_home/contact-button.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+
+const ContactButton = () => {
+  return (
+    <Button variant="ghost" size="sm">
+      Contact <ArrowRight />
+    </Button>
+  );
+};
+
+export default ContactButton;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 import ResumeButton from "@/components/shared/resume-button";
 import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
 import { H1, H3 } from "@/components/ui/typography";
-import { ArrowRight } from "lucide-react";
 import { CardsDemo } from "./_home/cards";
+import ContactButton from "./_home/contact-button";
 
 export default function Home() {
   return (
@@ -13,9 +12,7 @@ export default function Home() {
       <H3>I Can Build High Quality, Tested and Scalable Web Applications.</H3>
       <div className="flex items-center gap-2 mt-5">
         <ResumeButton />
-        <Button variant="ghost" size="sm">
-          Contact <ArrowRight />
-        </Button>
+        <ContactButton />
       </div>
 
       <CardsDemo />
