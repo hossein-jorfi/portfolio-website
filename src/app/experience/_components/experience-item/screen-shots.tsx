40ec107550e3ecceec49d25bb19512387ea7a18c
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-08-07 15:37:24 +0330

    ScreenShots

--- src/app/experience/_components/experience-item/screen-shots.tsx
+++ src/app/experience/_components/experience-item/screen-shots.tsx
@@ -4,34 +4,41 @@ import {
   Carousel,
   CarouselContent,
   CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
 } from "@/components/ui/carousel";
 import Image from "next/image";
 import Link from "next/link";
 
 const ScreenShots = ({ image, name }: { image: string; name: string }) => {
   return (
-    <Carousel
-      className="w-full"
-      opts={{
-        align: "start",
-      }}
-    >
-      <CarouselContent>
-        {Array.from({ length: 4 }).map((_, index) => (
-          <CarouselItem key={index} className="md:basis-1/2">
-            <Link href={image} target="_blank">
-              <Image
-                src={image || ""}
-                alt={name}
-                width={10000}
-                height={10000}
-                className="w-full h-auto object-cover rounded-lg border-2 cursor-pointer"
-              />
-            </Link>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-    </Carousel>
+    <div className="px-12">
+      <Carousel
+        className=""
+        opts={{
+          align: "start",
+        }}
+      >
+        <CarouselContent>
+          {Array.from({ length: 4 }).map((_, index) => (
+            <CarouselItem key={index} className="md:basis-1/2">
+              <Link href={image} target="_blank">
+                <Image
+                  src={image || ""}
+                  alt={name}
+                  width={10000}
+                  height={10000}
+                  className="w-full h-auto object-cover rounded-lg border-2 cursor-pointer"
+                />
+              </Link>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+
+        <CarouselPrevious className="left" />
+        <CarouselNext className="right" />
+      </Carousel>
+    </div>
   );
 };
 
